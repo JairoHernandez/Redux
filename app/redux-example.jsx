a1915369009662687137c5c0f72865e33ec9ff32
JairoHernandez <jairomh@hotmail.com>
2017-08-16 17:30:33 -0500

    Working Dispatch Handling actions

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -6,6 +6,7 @@ console.log('Starting redux example');
 // state passed is the value before a triggered change.
 var reducer = (state = {name: 'Anonymous'}, action) => {
     //state = state || {name: 'Anonymous'}; // ES5 version of passing in state = {name: 'Anonymous'}
+    console.log('New action:', action);
     return state;
 };
 
@@ -15,10 +16,11 @@ var store = redux.createStore(reducer);
 var currentState = store.getState();
 console.log('currentState', currentState);
 
-
-
-
-
+var action = {
+    type: 'CHANGE_NAME',
+    name: 'Andrew'
+};
+store.dispatch(action);
 
 
 
