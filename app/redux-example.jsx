7d2c642cc6e4367471ea0b58b7ec916e5b38def1
JairoHernandez <jairomh@hotmail.com>
2017-08-16 13:07:51 -0500

    Init commit

---
+++ app/redux-example.jsx
@@ -0,0 +1,60 @@
+var redux = require('redux');
+
+console.log('Starting redux example');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function changeProp(obj) {
+//     return {
+//         ...obj,
+//         name: 'Jen'
+//     }
+//     /** Causes both console.log to output Jen, which is not a pure fuction.*/
+//     //obj.name = 'Jen';
+//     //return obj;
+// }
+
+// var startingValue = {name: 'Andrew', age: 25};
+// var res = changeProp(startingValue);
+
+// console.log(startingValue);
+// console.log(res);
