a0215f379cbf4c5bb35cce71443e167c150c920b
JairoHernandez <jairomh@hotmail.com>
2017-08-16 13:46:52 -0500

    Add reducer and start redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,7 +3,17 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 
+// state passed is the value before a triggered change.
+var reducer = (state = {name: 'Anonymous'}, action) => {
+    //state = state || {name: 'Anonymous'}; // ES5 version of passing in state = {name: 'Anonymous'}
+    return state;
+};
 
+// Store consists of search text, completed fields, todos array.
+var store = redux.createStore(reducer);
+
+var currentState = store.getState();
+console.log('currentState', currentState);
 
 
 
