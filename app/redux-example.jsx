a6866443dabed8d724b7c1bc5b433998c552790a
JairoHernandez <jairomh@hotmail.com>
2017-08-17 17:32:59 -0500

    Add support for CHANGE_NAME and CHANGE_SEARC_TEXT action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,54 +2,28 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-
 // state passed is the value before a triggered change.
 var reducer = (state = {name: 'Anonymous'}, action) => {
     //state = state || {name: 'Anonymous'}; // ES5 version of passing in state = {name: 'Anonymous'}
-    console.log('New action:', action);
-    return state;
+    
+    console.log('New action:', action); // 1) New action: Object {type: "@@redux/INIT"} 3) New action: Object {type: "CHANGE_NAME", name: "Andrew"}
+    switch (action.type) {
+        case 'CHANGE_NAME':
+            return {...state, name: action.name};
+        default:
+            return state;
+    }
 };
 
 // Store consists of search text, completed fields, todos array.
 var store = redux.createStore(reducer);
-
 var currentState = store.getState();
-console.log('currentState', currentState);
+console.log('currentState', currentState); // 2) currentState Object {name: "Anonymous"}
 
-var action = {
-    type: 'CHANGE_NAME',
-    name: 'Andrew'
-};
-store.dispatch(action);
+// Triggers action
+store.dispatch({type: 'CHANGE_NAME', name: 'Andrew'});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log('Name should be andrew', store.getState()); // 4) Name should be andrew Object {name: "Anonymous"}
 
 
 
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,3 +1,7 @@
+// 1 Change the search text call it CHANGESEARCHTEXT
+// 2 Create reducer that handles CHANGESEARCHTEXT action type. It should update searchText property.
+// 3 Dispatch the action and console.log the new state.
+
 var redux = require('redux');
 
 console.log('Starting todo redux example')
@@ -9,9 +13,22 @@ var stateDefault = {
 };
 
 var reducer = (state=stateDefault, action) => {
-    return state;
+
+    switch (action.type) {
+        case 'CHANGE_SEARCH_TEXT':
+            return {...state, searchText: action.searchText};
+        default:
+            return state;
+    }
 };
 
 var store = redux.createStore(reducer);
 
 console.log('currentState:', store.getState());
+
+// Triggers action
+store.dispatch({type: 'CHANGE_SEARCH_TEXT', searchText: 'work'});
+console.log('Searchtext should be Funny boy', store.getState());
+
+
+
