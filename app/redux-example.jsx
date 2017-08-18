0afb49b7afd958e2041c66025d6cddbbadba129d
JairoHernandez <jairomh@hotmail.com>
2017-08-17 23:54:33 -0500

    Add subscribe and dev tools

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -6,7 +6,7 @@ console.log('Starting redux example');
 var reducer = (state = {name: 'Anonymous'}, action) => {
     //state = state || {name: 'Anonymous'}; // ES5 version of passing in state = {name: 'Anonymous'}
     
-    console.log('New action:', action); // 1) New action: Object {type: "@@redux/INIT"} 3) New action: Object {type: "CHANGE_NAME", name: "Andrew"}
+    // console.log('New action:', action); // 1) New action: Object {type: "@@redux/INIT"} 3) New action: Object {type: "CHANGE_NAME", name: "Andrew"}
     switch (action.type) {
         case 'CHANGE_NAME':
             return {...state, name: action.name};
@@ -16,15 +16,25 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
 };
 
 // Store consists of search text, completed fields, todos array.
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes to your state. Function called everytime state changes.
+var unsubscribe = store.subscribe(() => {
+    var state = store.getState(); // Fires everytime state changes.
+
+    console.log('Name is', state.name); // 4) Name should be andrew Object {name: "Anonymous"}
+    document.getElementById('app').innerHTML = state.name;
+});
+
 var currentState = store.getState();
 console.log('currentState', currentState); // 2) currentState Object {name: "Anonymous"}
 
-// Triggers action
+// Triggers action to change state.
 store.dispatch({type: 'CHANGE_NAME', name: 'Andrew'});
-
-console.log('Name should be andrew', store.getState()); // 4) Name should be andrew Object {name: "Anonymous"}
-
+// unsubscribe();
+store.dispatch({type: 'CHANGE_NAME', name: 'Emily'});
 
 
 
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -22,13 +22,25 @@ var reducer = (state=stateDefault, action) => {
     }
 };
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// subscribe to changes
+store.subscribe(() => {
+    var state = store.getState(); // Fires everytime state changes.
+
+    document.getElementById('app').innerHTML = state.searchText;
+});
 
 console.log('currentState:', store.getState());
 
-// Triggers action
+// Triggers action to change state.
 store.dispatch({type: 'CHANGE_SEARCH_TEXT', searchText: 'work'});
-console.log('Searchtext should be Funny boy', store.getState());
+// console.log('Searchtext should be Funny boy', store.getState());
+store.dispatch({type: 'CHANGE_SEARCH_TEXT', searchText: 'dog'});
+store.dispatch({type: 'CHANGE_SEARCH_TEXT', searchText: 'Something else'});
+
 
 
 
